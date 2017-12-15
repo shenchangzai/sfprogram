91d0d7ea3fd7c918ccf578d03721d399c21ac076
1 <1>
2017-12-15 11:00:19 +0800

    代码提交

--- common-core/src/main/java/com/xu/common/model/Anybus.java
+++ common-core/src/main/java/com/xu/common/model/Anybus.java
@@ -7,26 +7,75 @@ public class Anybus implements BaseModel<String>{
 	 */
 	private static final long serialVersionUID = 2436881208070091162L;
 	
-	private String key;
-	private String name;
-	private int age;
-	public String getKey() {
-		return key;
+	private String mktId;
+	private String mktNameShow;
+	private int dailyMinPackages;
+	private double weightMin;
+	private double weightMax;
+	private double basePrice;
+	private double baseWeight;
+	private int groupLimit;
+	private int groupDuration;
+	private String useRequire;
+	public String getMktId() {
+		return mktId;
 	}
-	public void setKey(String key) {
-		this.key = key;
+	public void setMktId(String mktId) {
+		this.mktId = mktId;
 	}
-	public String getName() {
-		return name;
+	public String getMktNameShow() {
+		return mktNameShow;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setMktNameShow(String mktNameShow) {
+		this.mktNameShow = mktNameShow;
 	}
-	public int getAge() {
-		return age;
+	public int getDailyMinPackages() {
+		return dailyMinPackages;
 	}
-	public void setAge(int age) {
-		this.age = age;
+	public void setDailyMinPackages(int dailyMinPackages) {
+		this.dailyMinPackages = dailyMinPackages;
+	}
+	public double getWeightMin() {
+		return weightMin;
+	}
+	public void setWeightMin(double weightMin) {
+		this.weightMin = weightMin;
+	}
+	public double getWeightMax() {
+		return weightMax;
+	}
+	public void setWeightMax(double weightMax) {
+		this.weightMax = weightMax;
+	}
+	public double getBasePrice() {
+		return basePrice;
+	}
+	public void setBasePrice(double basePrice) {
+		this.basePrice = basePrice;
+	}
+	public double getBaseWeight() {
+		return baseWeight;
+	}
+	public void setBaseWeight(double baseWeight) {
+		this.baseWeight = baseWeight;
+	}
+	public int getGroupLimit() {
+		return groupLimit;
+	}
+	public void setGroupLimit(int groupLimit) {
+		this.groupLimit = groupLimit;
+	}
+	public int getGroupDuration() {
+		return groupDuration;
+	}
+	public void setGroupDuration(int groupDuration) {
+		this.groupDuration = groupDuration;
+	}
+	public String getUseRequire() {
+		return useRequire;
+	}
+	public void setUseRequire(String useRequire) {
+		this.useRequire = useRequire;
 	}
 	@Override
 	public String getId() {
